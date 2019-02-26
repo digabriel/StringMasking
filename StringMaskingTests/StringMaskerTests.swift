f82a2dde7452917edfdf30e86b7af99ef1e765ef
Dimas Gabriel <dimas@dimasgabriel.net>
2019-02-26 08:49:32 -0300

    Tests refactoring

--- StringMaskingTests/StringMaskerTests.swift
+++ StringMaskingTests/StringMaskerTests.swift
@@ -23,51 +23,44 @@ class StringMaskingTests: XCTestCase {
     }
     
     func test_onlyNumbersMask_mapNumberStringCorrectly() {
-        let mask = "##"
         let stringToMask = "12"
-        let sut = StringMasker(mask: mask)
+        let sut = StringMasker(mask: "##")
         XCTAssertEqual(sut.mask(stringToMask), "12")
     }
     
     func test_maskBiggerThanString_mapsCorrectly() {
-        let mask = "###"
         let stringToMask = "12"
-        let sut = StringMasker(mask: mask)
+        let sut = StringMasker(mask: "###")
         XCTAssertEqual(sut.mask(stringToMask), "12")
     }
     
     func test_maskSmallerThanString_mapsCorrectly() {
-        let mask = "#+##"
         let stringToMask = "12345"
-        let sut = StringMasker(mask: mask)
+        let sut = StringMasker(mask: "#+##")
         XCTAssertEqual(sut.mask(stringToMask), "1+23")
     }
 
     func test_maskWithSymbols_mapsCorrectly() {
-        let mask = "+# ###"
         let stringToMask = "1234"
-        let sut = StringMasker(mask: mask)
+        let sut = StringMasker(mask: "+# ###")
         XCTAssertEqual(sut.mask(stringToMask), "+1 234")
     }
     
     func test_stringWithNonNumbersCharacters_mapsCorrectly() {
-        let mask = "+# ###"
         let stringToMask = "12aa567"
-        let sut = StringMasker(mask: mask)
+        let sut = StringMasker(mask: "+# ###")
         XCTAssertEqual(sut.mask(stringToMask), "+1 2")
     }
     
     func test_phoneMask_mapsPhoneNumber() {
-        let mask = "+# (###) ###-####"
         let phoneNumber = "18005550123"
-        let sut = StringMasker(mask: mask)
+        let sut = StringMasker(mask: "+# (###) ###-####")
         XCTAssertEqual(sut.mask(phoneNumber), "+1 (800) 555-0123")
     }
     
     func test_maskedString_removesSymbols() {
-        let mask = "+# (###) ###-####"
         let phoneNumber = "+1 (800) 555-0123"
-        let sut = StringMasker(mask: mask)
+        let sut = StringMasker(mask: "+# (###) ###-####")
         XCTAssertEqual(sut.rawString(from: phoneNumber), "18005550123")
     }
     
